ccc82dbd06f5e4ff69bd7b842775dcf04d04618b
varung97 <varung97@gmail.com>
2019-04-28 09:56:18 +0800

    Move questions and get angular working

--- app.py
+++ app.py
@@ -4,13 +4,27 @@ import json
 
 app = Flask(__name__)
 
+jinja_options = app.jinja_options.copy()
+
+jinja_options.update(dict(
+    block_start_string='<%',
+    block_end_string='%>',
+    variable_start_string='%%',
+    variable_end_string='%%',
+    comment_start_string='<#',
+    comment_end_string='#>'
+))
+app.jinja_options = jinja_options
 
 @app.route('/')
 def index():
     return render_template("index.html")
 
+@app.route('/physics')
+def physics():
+    return render_template("physics.html")
+
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port, debug=True)
-
